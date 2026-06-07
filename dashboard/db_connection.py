4ef9d1919b2535ee3e2c657a5b9c2544b0dbe810
Sahil G <sahiillvg@gmail.com>
2026-06-07 21:31:24 +0530

    Remove hardcoded database password

--- dashboard/db_connection.py
+++ dashboard/db_connection.py
@@ -1,9 +1,10 @@
 import mysql.connector
+import os
 
 def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="230623",
+        password=os.getenv("MYSQL_PASSWORD"),
         database="supply_chain_risk_intelligence"
     )
